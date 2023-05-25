4295a1310b9b7b02a06867868c1931c17d7abb57
혀느현스 <hyuns@hyuns.dev>
2023-05-25 12:25:41 +0900

    feat: add delay

--- components/work.py
+++ components/work.py
@@ -8,8 +8,9 @@ WORK_GAP = 30
 WORK_COUNT = 4
 
 
-def getX(order, all = WORK_COUNT, gap = WORK_GAP):
+def getX(order, all=WORK_COUNT, gap=WORK_GAP):
     return (p5Context.width / 2 - 100) + ((order) - (all - 1) / 2) * (WORK_SIZE + gap)
+
 
 class WorkComponent(Element):
     def __init__(self, work, order):
@@ -20,11 +21,13 @@ class WorkComponent(Element):
         self.isFoucs = False
 
         self.backgroundArea = Area(getX(order), 250, WORK_SIZE, WORK_SIZE)
-        self.imageArea = Area(getX(order) + 10, 250 + 10, WORK_SIZE - 20, WORK_SIZE - 20)
+        self.imageArea = Area(
+            getX(order) + 10, 250 + 10, WORK_SIZE - 20, WORK_SIZE - 20
+        )
 
-        self.addEventListener('mousePressed', self.__mousePressedHandler)
-        self.addEventListener('mouseOver', self.__mouseOverHandler)
-        self.addEventListener('mouseOut', self.__mouseOutHandler)
+        self.addEventListener("mousePressed", self.__mousePressedHandler)
+        self.addEventListener("mouseOver", self.__mouseOverHandler)
+        self.addEventListener("mouseOut", self.__mouseOutHandler)
 
     def draw(self):
         self.__tick()
@@ -41,7 +44,6 @@ class WorkComponent(Element):
         self.backgroundArea.tick()
         self.imageArea.tick()
 
-
     def __mousePressedHandler(self, event):
         if self.isFoucs:
             self.__unfocus()
@@ -49,12 +51,18 @@ class WorkComponent(Element):
             self.__focus()
 
     def __mouseOverHandler(self, event):
-        if self.isFoucs: return
-        self.backgroundArea.transition((self.x - 10, self.y - 10, WORK_SIZE + 20, WORK_SIZE + 20), 12, (0, 1, 0, 1))
+        if self.isFoucs:
+            return
+        self.backgroundArea.transition(
+            (self.x - 10, self.y - 10, WORK_SIZE + 20, WORK_SIZE + 20), 12, (0, 1, 0, 1)
+        )
 
     def __mouseOutHandler(self, event):
-        if self.isFoucs: return
-        self.backgroundArea.transition((self.x, self.y, WORK_SIZE, WORK_SIZE), 12, (0, 1, 0, 1))
+        if self.isFoucs:
+            return
+        self.backgroundArea.transition(
+            (self.x, self.y, WORK_SIZE, WORK_SIZE), 12, (0, 1, 0, 1)
+        )
 
     def __focus(self):
         self.isFoucs = True
@@ -63,29 +71,33 @@ class WorkComponent(Element):
         backgroundTarget = (0, 0, p5Context.width, p5Context.height)
         self.backgroundArea.transition(backgroundTarget, 12, (0, 1, 0, 1))
 
-        imageTarget = (p5Context.width / 2 - WORK_SIZE / 2, p5Context.height / 2 - WORK_SIZE / 2, WORK_SIZE, WORK_SIZE)
-        self.imageArea.transition(imageTarget, 12, (0, 1, 0, 1))
+        imageTarget = (
+            p5Context.width / 2 - WORK_SIZE / 2,
+            p5Context.height / 2 - WORK_SIZE / 2,
+            WORK_SIZE,
+            WORK_SIZE,
+        )
+        self.imageArea.transition(imageTarget, 10, (0, 1, 0, 1), 2)
 
-        self.x = 0 
+        self.x = 0
         self.y = 0
         self.width = p5Context.width
         self.height = p5Context.height
-
 
     def __unfocus(self):
         self.isFoucs = False
         self.z_index = 0
 
-        self.backgroundArea.transition((getX(self.order), 250, WORK_SIZE, WORK_SIZE), 12, (0, 1, 0, 1))
-        self.imageArea.transition((getX(self.order) + 10, 250 + 10, WORK_SIZE - 20, WORK_SIZE - 20), 12, (0, 1, 0, 1))
+        self.backgroundArea.transition(
+            (getX(self.order), 250, WORK_SIZE, WORK_SIZE), 12, (0, 1, 0, 1)
+        )
+        self.imageArea.transition(
+            (getX(self.order) + 10, 250 + 10, WORK_SIZE - 20, WORK_SIZE - 20),
+            12,
+            (0, 1, 0, 1),
+        )
 
         self.x = getX(self.order)
         self.y = 250
         self.width = WORK_SIZE
         self.height = WORK_SIZE
-
-
-
-
-
-
--- element/area.py
+++ element/area.py
@@ -1,5 +1,6 @@
 from .animatedValue import AnimatedValue
 
+
 class Area(object):
     def __init__(self, x, y, width, height):
         self.x = AnimatedValue(x)
@@ -13,14 +14,17 @@ class Area(object):
         self.width.tick()
         self.height.tick()
 
-    def transition(self, target, duration, timing):
-        self.x.transition(target[0], duration, timing)
-        self.y.transition(target[1], duration, timing)
-        self.width.transition(target[2], duration, timing)
-        self.height.transition(target[3], duration, timing)
+    def transition(self, target, duration, timing, delay=0):
+        self.x.transition(target[0], duration, timing, delay)
+        self.y.transition(target[1], duration, timing, delay)
+        self.width.transition(target[2], duration, timing, delay)
+        self.height.transition(target[3], duration, timing, delay)
 
     @property
     def hitbox(self):
-        return (self.x.value, self.y.value, int(self.width.value), int(self.height.value))
-    
-
+        return (
+            self.x.value,
+            self.y.value,
+            int(self.width.value),
+            int(self.height.value),
+        )
